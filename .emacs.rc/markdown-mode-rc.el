3897b8edc5e0f87ed33b710c7b7ad454efed4ee1
rexim <reximkut@gmail.com>
2013-05-04 19:17:24 +0700

    markdown-mode: set markdown-command variable for gnu/linux system.

--- .emacs.rc/markdown-mode-rc.el
+++ .emacs.rc/markdown-mode-rc.el
@@ -4,3 +4,6 @@
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(when (eq system-type 'gnu/linux)
+  (setq markdown-command "markdown_py"))
