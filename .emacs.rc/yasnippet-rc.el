4551d6dea87d19cda962140486bf421b5a032a25
rexim <reximkut@gmail.com>
2013-05-09 21:24:34 +0700

    I don't like those clojure snippets.

--- .emacs.rc/yasnippet-rc.el
+++ .emacs.rc/yasnippet-rc.el
@@ -1,5 +1,4 @@
 (rc/ensure-package-installed 'yasnippet)
-(rc/ensure-package-installed 'clojure-snippets)
 
 (defun rc/turn-on-yasnippets ()
   (interactive)
@@ -7,4 +6,3 @@
 
 (add-hook 'emacs-lisp-mode-hook 'rc/turn-on-yasnippets)
 (add-hook 'c++-mode-hook        'rc/turn-on-yasnippets)
-(add-hook 'clojure-mode-hook    'rc/turn-on-yasnippets)
