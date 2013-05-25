7dec1c7d7b59dd2dd56846eb5688bf37fde570e4
rexim <reximkut@gmail.com>
2013-05-25 11:54:01 +0700

    Turned on rainbow-mode for html, css, and js modes.

--- .emacs.rc/rainbow-rc.el
+++ .emacs.rc/rainbow-rc.el
@@ -1,3 +1,11 @@
 (rc/ensure-package-installed 'rainbow-mode)
 ;; (rc/ensure-package-installed 'rainbow-delimiters)
 ;; (global-rainbow-delimiters-mode)
+
+(defun rc/turn-on-rainbow-mode ()
+  (interactive)
+  (rainbow-mode 1))
+
+(add-hook 'html-mode-hook 'rc/turn-on-rainbow-mode)
+(add-hook 'css-mode-hook  'rc/turn-on-rainbow-mode)
+(add-hook 'js-mode-hook   'rc/turn-on-rainbow-mode)
