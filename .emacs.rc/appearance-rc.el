c106675246a2541d73762984ebe162ffb6bc1b14
rexim <reximkut@gmail.com>
2013-05-31 23:29:28 +0700

    Zenburn as default theme.

--- .emacs.rc/appearance-rc.el
+++ .emacs.rc/appearance-rc.el
@@ -11,8 +11,8 @@
 (show-paren-mode 1)
 
 ;;; Color theme
-(rc/ensure-package-installed 'gruber-darker-theme)
-(load-theme 'gruber-darker t)
+;; (rc/ensure-package-installed 'gruber-darker-theme)
+;; (load-theme 'gruber-darker t)
 
-;; (rc/ensure-package-installed 'zenburn-theme)
-;; (load-theme 'zenburn t)
+(rc/ensure-package-installed 'zenburn-theme)
+(load-theme 'zenburn t)
