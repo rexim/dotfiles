9fafecf83878a18ebd0802f720f2e832c52388e7
rexim <reximkut@gmail.com>
2013-06-12 10:36:29 +0700

    markdown-mode-rc: removed autoload code. Seems markdown finally does it by itself.

--- .emacs.rc/markdown-mode-rc.el
+++ .emacs.rc/markdown-mode-rc.el
@@ -1,9 +1,4 @@
 (rc/ensure-package-installed 'markdown-mode)
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 (when (eq system-type 'gnu/linux)
   (setq markdown-command "markdown_py"))
