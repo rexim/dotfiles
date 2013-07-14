5f22ad9eee66ea79b168144f5610639e049553d4
rexim <reximkut@gmail.com>
2013-07-14 17:21:40 +0700

    slime-rc: finally got it working.

--- .emacs.rc/slime-rc.el
+++ .emacs.rc/slime-rc.el
@@ -1,1 +1,6 @@
 (rc/ensure-package-installed 'slime)
+(require 'slime-autoloads)
+;; WARNING! If you're experiencing a problem with initialization of
+;; SLIME, remove all its *.elc files.
+(slime-setup '(slime-fancy slime-banner))
+(setq inferior-lisp-program "/usr/bin/sbcl")
