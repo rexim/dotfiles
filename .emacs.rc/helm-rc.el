7daf11f24cac5bd34868216b55741cc2d8e65bb3
rexim <reximkut@gmail.com>
2013-10-07 10:54:50 +0700

    Added some binding for the helm commands.

--- .emacs.rc/helm-rc.el
+++ .emacs.rc/helm-rc.el
@@ -1,3 +1,6 @@
 (rc/ensure-package-installed 'helm)
 (rc/ensure-package-installed 'helm-cmd-t)
 (rc/ensure-package-installed 'helm-git-grep)
+
+(global-set-key (kbd "C-c h t") 'helm-cmd-t)
+(global-set-key (kbd "C-c h g") 'helm-git-grep)
