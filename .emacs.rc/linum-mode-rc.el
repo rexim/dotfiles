d2598aaf24cead6772f8d198c3683141cbc2c717
rexim <reximkut@gmail.com>
2013-10-08 07:17:54 +0700

    Improved linum-mode-rc.

--- .emacs.rc/linum-mode-rc.el
+++ .emacs.rc/linum-mode-rc.el
@@ -2,9 +2,15 @@
   (interactive)
   (linum-mode 1))
 
-(add-hook 'c-mode-common-hook   'rc/turn-on-linum-mode)
-(add-hook 'emacs-lisp-mode-hook 'rc/turn-on-linum-mode)
-(add-hook 'haskell-mode-hook    'rc/turn-on-linum-mode)
-(add-hook 'perl-mode-hook       'rc/turn-on-linum-mode)
-(add-hook 'python-mode-hook     'rc/turn-on-linum-mode)
-(add-hook 'asm-mode-hook        'rc/turn-on-linum-mode)
+(setq rc/linum-compatible-modes
+      '(c-mode-common-hook
+        emacs-lisp-mode-hook
+        haskell-mode-hook
+        perl-mode-hook
+        python-mode-hook
+        asm-mode-hook
+        html-mode-hook
+        css-mode-hook))
+
+(dolist (mode rc/linum-compatible-modes)
+  (add-hook mode 'rc/turn-on-linum-mode))
