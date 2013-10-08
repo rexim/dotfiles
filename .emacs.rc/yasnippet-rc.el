556151bdedd1bbc4ca37a991978aceea5712c2ad
rexim <reximkut@gmail.com>
2013-10-08 21:45:40 +0700

    Improved yasnippet-rc.

--- .emacs.rc/yasnippet-rc.el
+++ .emacs.rc/yasnippet-rc.el
@@ -12,13 +12,17 @@
 (eval-after-load 'yasnippet
   (yas-reload-all))
 
-(defun rc/turn-on-yasnippets ()
+(defun rc/turn-on-yasnippet ()
   (interactive)
   (yas-minor-mode 1))
 
-(add-hook 'emacs-lisp-mode-hook 'rc/turn-on-yasnippets)
-(add-hook 'c-mode-common-hook   'rc/turn-on-yasnippets)
-(add-hook 'scala-mode-hook      'rc/turn-on-yasnippets)
-(add-hook 'js-mode-hook         'rc/turn-on-yasnippets)
-(add-hook 'html-mode-hook       'rc/turn-on-yasnippets)
-(add-hook 'nxml-mode-hook       'rc/turn-on-yasnippets)
+(setq rc/yasnippet-compatible-mode-hooks
+      '(emacs-lisp-mode-hook
+        c-mode-common-hook
+        scala-mode-hook
+        js-mode-hook
+        html-mode-hook
+        nxml-mode-hook))
+
+(dolist (mode-hook rc/yasnippet-compatible-mode-hooks)
+  (add-hook mode-hook 'rc/turn-on-yasnippet))
