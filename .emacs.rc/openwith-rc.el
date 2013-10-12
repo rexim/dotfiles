1dd5515590c3dbaa9132abe15334de543f4e76bc
rexim <reximkut@gmail.com>
2013-10-12 13:34:43 +0700

    Open djvu files through evince.

--- .emacs.rc/openwith-rc.el
+++ .emacs.rc/openwith-rc.el
@@ -5,6 +5,7 @@
 (when (eq system-type 'gnu/linux)
   (setq openwith-associations
         '(("\\.pdf\\'" "evince" (file))
+          ("\\.djvu\\'" "evince" (file))
 
           ;; FIXME(rexim): it's probably possible to merge this two
           ;; entries.
