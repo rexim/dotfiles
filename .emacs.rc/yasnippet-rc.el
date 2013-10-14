f7624272aa11fdd2c2c02d8729b018d0efdcc7e7
rexim <reximkut@gmail.com>
2013-10-14 09:02:25 +0700

    Using ys-global-mode.

--- .emacs.rc/yasnippet-rc.el
+++ .emacs.rc/yasnippet-rc.el
@@ -8,23 +8,4 @@
 (setq yas/prompt-functions '(yas/dropdown-prompt))
 (setq yas-snippet-dirs '("~/.emacs.snippets/"))
 
-(eval-after-load 'yasnippet
-  (yas-reload-all))
-
-(defun rc/turn-on-yasnippet ()
-  (interactive)
-  (yas-minor-mode 1))
-
-(setq rc/yasnippet-compatible-mode-hooks
-      '(emacs-lisp-mode-hook
-        c-mode-common-hook
-        scala-mode-hook
-        js-mode-hook
-        html-mode-hook
-        nxml-mode-hook
-        css-mode-hook
-        latex-mode-hook
-        markdown-mode-hook))
-
-(dolist (mode-hook rc/yasnippet-compatible-mode-hooks)
-  (add-hook mode-hook 'rc/turn-on-yasnippet))
+(yas-global-mode 1)
