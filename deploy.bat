43744f285cf31ab9db23c0ff7bcda28014dee6cc
rexim <reximkut@gmail.com>
2013-10-18 11:52:12 +0700

    Original names

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 @echo off
 set SCRIPT_DIR=%~dp0
 
-mklink %APPDATA%\.emacs %SCRIPT_DIR%emacs.init
-mklink /D %APPDATA%\.emacs.rc %SCRIPT_DIR%emacs.rc
-mklink /D %APPDATA%\.emacs.snippets %SCRIPT_DIR%emacs.snippets
+mklink %APPDATA%\.emacs %SCRIPT_DIR%.emacs
+mklink /D %APPDATA%\.emacs.rc %SCRIPT_DIR%.emacs.rc
+mklink /D %APPDATA%\.emacs.snippets %SCRIPT_DIR%.emacs.snippets
