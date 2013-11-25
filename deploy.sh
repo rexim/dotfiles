545d3d38ac7ed4e235b893dd8d4b4b38c7999533
rexim <reximkut@gmail.com>
2013-11-25 16:37:57 +0700

    Improved deploy script for linux

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+FILES="
+.emacs
+.emacs.rc
+.emacs.snippets
+"
 
-ln -s $SCRIPT_DIR/.emacs ~/.emacs
-ln -s $SCRIPT_DIR/.emacs.rc ~/.emacs.rc
-ln -s $SCRIPT_DIR/.emacs.snippets ~/.emacs.snippets
+for f in $FILES; do
+    if [ -L ~/$f ]; then
+	echo "[WARNING] $f is already deployed"
+    else
+	ln -s $SCRIPT_DIR/$f ~/$f
+	echo "[OK] $f has been deployed"
+    fi
+done
