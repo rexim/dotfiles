7cf655df58b8ec07ff093b5b5786b8cf16e85919
rexim <reximkut@gmail.com>
2013-12-10 10:42:33 +0700

    helm: don't show only basename.

--- .emacs.rc/helm-rc.el
+++ .emacs.rc/helm-rc.el
@@ -2,5 +2,7 @@
 (rc/ensure-package-installed 'helm-cmd-t)
 (rc/ensure-package-installed 'helm-git-grep)
 
+(setq helm-ff-transformer-show-only-basename nil)
+
 (global-set-key (kbd "C-c h t") 'helm-cmd-t)
 (global-set-key (kbd "C-c h g") 'helm-git-grep)
