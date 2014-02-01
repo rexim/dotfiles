aab2a4d3e0ad5fe17f42f2f94903703db572ec49
rexim <reximkut@gmail.com>
2014-02-02 00:18:29 +0700

    rc/org-link-from-clipboarded-url: dramatically improved the title extraction.

--- .emacs.rc/misc-rc.el
+++ .emacs.rc/misc-rc.el
@@ -35,16 +35,30 @@
         (kill-buffer buffer)
         (message "Killed autoloads buffer %s" name)))))
 
+(defun rc/straight-string (s)
+  (mapconcat '(lambda (x) x)
+             (split-string s)
+             " "))
+
 (defun rc/prepare-org-link-title (title)
   (let ((replace-table '(("\\[" . "{")
-                         ("\\]" . "}")))
+                         ("\\]" . "}")
+                         ("&mdash;" . "—")))
         (max-length 77)
-        (result title))
+        (result (rc/straight-string title)))
     (dolist (x replace-table)
       (setq result (replace-regexp-in-string (car x) (cdr x) result)))
     (when (> (length result) max-length)
       (setq result (concat (substring result 0 max-length) "...")))
     result))
+
+(defun rc/extract-title-from-html (html)
+  (let ((start (string-match "<title>" html))
+        (end (string-match "</title>" html))
+        (chars-to-skip (length "<title>")))
+    (if (and start end (< start end))
+        (substring html (+ start chars-to-skip) end)
+      nil)))
 
 (defun rc/org-link-from-clipboarded-url ()
   (interactive)
@@ -53,12 +67,12 @@
     (url-retrieve
      url
      `(lambda (s)
-        (let ((content (decode-coding-string (buffer-string) 'utf-8)))
-          (string-match "<title>[[:space:]\n]*\\(.*\\)[[:space:]\n]*</title>"
-                        content)
-          (let ((title (rc/prepare-org-link-title (match-string 1 content))))
-            (with-current-buffer ,dest-buffer
-              (insert (format "[[%s][%s]]" ,url title)))))))))
+        (let* ((content (decode-coding-string
+                         (buffer-string) 'utf-8))
+               (title (rc/prepare-org-link-title
+                       (rc/extract-title-from-html content))))
+          (with-current-buffer ,dest-buffer
+            (insert (format "[[%s][%s]]" ,url title))))))))
 
 (global-set-key (kbd "C-x p i") 'rc/org-link-from-clipboarded-url)
 
