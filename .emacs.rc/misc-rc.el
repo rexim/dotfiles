d3c9193435ff91d1fa786d3e65ed0b9e0146033d
rexim <reximkut@gmail.com>
2014-03-26 12:42:00 +0700

    Added useful function for XML pretty printing.

--- .emacs.rc/misc-rc.el
+++ .emacs.rc/misc-rc.el
@@ -41,3 +41,20 @@
                  "*Simple Python HTTP Server*"))
 
 (global-set-key (kbd "C-x p s") 'rc/start-python-simple-http-server)
+
+;;; Taken from here:
+;;; http://blog.bookworm.at/2007/03/pretty-print-xml-with-emacs.html
+(defun bf-pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t) 
+        (backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!"))
