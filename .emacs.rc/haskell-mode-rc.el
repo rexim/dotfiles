04027fc3e8a2b78657a98d7ffe586ce86ae7a810
rexim <reximkut@gmail.com>
2014-07-03 11:24:19 +0700

    haskell-mode: updated some hooks.

--- .emacs.rc/haskell-mode-rc.el
+++ .emacs.rc/haskell-mode-rc.el
@@ -1,6 +1,5 @@
 (rc/ensure-package-installed 'haskell-mode)
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+(add-hook 'haskell-mode-hook 'haskell-indent-mode)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'haskell-mode-hook 'haskell-doc-mode)
