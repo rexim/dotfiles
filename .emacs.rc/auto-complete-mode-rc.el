09441f9196f050cdb1b169b33f1808c5acf0dff8
rexim <reximkut@gmail.com>
2014-12-08 07:58:40 +0600

    Using global-auto-complete-minor-mode

--- .emacs.rc/auto-complete-mode-rc.el
+++ .emacs.rc/auto-complete-mode-rc.el
@@ -2,10 +2,9 @@
 
 (require 'auto-complete)
 (require 'auto-complete-config)
+
 (ac-config-default)
 
-(defun rc/turn-on-auto-complete-mode ()
-  (auto-complete-mode 1))
+(global-auto-complete-mode 1)
 
-(add-hook 'c-mode-common-hook 'rc/turn-on-auto-complete-mode)
-(add-hook 'emacs-lisp-mode-hook 'rc/turn-on-auto-complete-mode)
+
