c2a195eff1e765122508d44a5f011bf56188a7ed
rexim <reximkut@gmail.com>
2014-12-08 08:07:12 +0600

    Add ac-haskell-process.

--- .emacs.rc/auto-complete-mode-rc.el
+++ .emacs.rc/auto-complete-mode-rc.el
@@ -1,10 +1,15 @@
 (rc/require 'auto-complete)
+(rc/require 'ac-haskell-process)
 
 (require 'auto-complete)
 (require 'auto-complete-config)
+(require 'ac-haskell-process)
 
 (ac-config-default)
 
 (global-auto-complete-mode 1)
 
-
+(add-hook 'interactive-haskell-mode-hook 'ac-haskell-process-setup)
+(add-hook 'haskell-interactive-hook 'ac-haskell-process-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'haskell-interactive-mode))
