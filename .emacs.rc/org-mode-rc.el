30d73820c37f4f12c4be6631ee9bf2b3e19f1b2c
rexim <reximkut@gmail.com>
2014-12-15 15:59:48 +0600

    Added macro for inserting comments with silent timestamp,.

--- .emacs.rc/org-mode-rc.el
+++ .emacs.rc/org-mode-rc.el
@@ -7,3 +7,9 @@
 (setq org-agenda-files (list "~/Documents/Personal/Tasks.org"
                              "~/Documents/Personal/Imported.org"
                              "~/Documents/Personal/Archive.org"))
+
+(defun rc/org-comment (&optional arg)
+  (interactive "p")
+  (kmacro-exec-ring-item (quote ([3 33 return 134217730 32 2] 0 "%d")) arg))
+
+(global-set-key (kbd "C-x p c") 'rc/org-comment)
