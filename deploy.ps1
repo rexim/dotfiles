283a22fe10089195714c0922e4be2baa7658cfa5
rexim <reximkut@gmail.com>
2015-02-15 15:07:07 +0300

    Implemented deploy script for Windows. Close #1

---
+++ deploy.ps1
@@ -0,0 +1,26 @@
+﻿$Manifest = Import-Csv -Header ("file", "operation") -Delimiter ("|") -Path .\MANIFEST
+$EmacsHome = Get-ChildItem Env:\APPDATA | %{ $_.VALUE }
+foreach ($ManifestRow in $Manifest) {
+    $DeployFile = $ManifestRow.file
+    $DeployOp = $ManifestRow.operation
+    $SourcePath = "$PSScriptRoot\$DeployFile"
+    $DestPath = "$EmacsHome\$DeployFile"
+    switch($DeployOp) {
+        "symlink" {
+            if ((Get-Item $SourcePath) -is [System.IO.DirectoryInfo]) {
+                cmd /c mklink /D "$DestPath" "$SourcePath"
+            } else {
+                cmd /c mklink "$DestPath" "$SourcePath"
+            }
+            echo "$DeployFile has been symlinked"
+        }
+
+        "copy" {
+            Write-Warning "The 'copy' operation is not implemented yet. Skipping..."
+        }
+
+        default {
+            Write-Warning "Unknown operation $operation. Skipping..."
+        }
+    }
+}
