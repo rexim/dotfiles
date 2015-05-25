3fd9380e8669759ecbe49eb2dfa7ff994579b6ab
rexim <reximkut@gmail.com>
2015-05-25 13:14:46 +0600

    elfeed: add dsavenko.com

--- .emacs.rc/elfeed-rc.el
+++ .emacs.rc/elfeed-rc.el
@@ -1,5 +1,6 @@
 (rc/require 'elfeed)
 (setq elfeed-feeds
       '(("http://githubengineering.com/atom.xml")
-        ("https://fornever.me/rss.xml")))
+        ("https://fornever.me/rss.xml")
+        ("http://dsavenko.com/atom.xml")))
 (global-set-key (kbd "C-x w") 'elfeed)
