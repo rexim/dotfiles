88cbb19e7f85496dd63f211ef4db0fc4d1178693
rexim <reximkut@gmail.com>
2015-05-25 17:02:01 +0600

    elfeed: add a couple of blogs

--- .emacs.rc/elfeed-rc.el
+++ .emacs.rc/elfeed-rc.el
@@ -2,5 +2,7 @@
 (setq elfeed-feeds
       '(("http://githubengineering.com/atom.xml")
         ("https://fornever.me/rss.xml")
-        ("http://dsavenko.com/atom.xml")))
+        ("http://dsavenko.com/atom.xml")
+        ("http://feeds.feedburner.com/xoredhq")
+        ("http://blog.4geo.ru/feeds/posts/default")))
 (global-set-key (kbd "C-x w") 'elfeed)
