e77c2c0b9d1f2fc831a022780f7109bf4f366eb4
rexim <reximkut@gmail.com>
2015-05-26 10:36:24 +0600

    elfeed: add eevee's blog

--- .emacs.rc/elfeed-rc.el
+++ .emacs.rc/elfeed-rc.el
@@ -4,5 +4,6 @@
         ("https://fornever.me/rss.xml")
         ("http://dsavenko.com/atom.xml")
         ("http://feeds.feedburner.com/xoredhq")
-        ("http://blog.4geo.ru/feeds/posts/default")))
+        ("http://blog.4geo.ru/feeds/posts/default")
+        ("http://eev.ee/feeds/atom.xml")))
 (global-set-key (kbd "C-x w") 'elfeed)
