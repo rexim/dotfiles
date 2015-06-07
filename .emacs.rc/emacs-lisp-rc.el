67ee870a7f47e09b4f5622bb9df915ad5afd7662
rexim <reximkut@gmail.com>
2015-06-07 17:25:14 +0600

    Use emacs-lisp-mode for Cask files

--- .emacs.rc/emacs-lisp-rc.el
+++ .emacs.rc/emacs-lisp-rc.el
@@ -2,3 +2,4 @@
           '(lambda ()
              (local-set-key (kbd "C-c C-j")
                             (quote eval-print-last-sexp))))
+(add-to-list 'auto-mode-alist '("Cask" . emacs-lisp-mode))
