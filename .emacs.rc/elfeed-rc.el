29197bbd3f9eb4c622f1536d5126644a19fe3d16
rexim <reximkut@gmail.com>
2015-06-15 17:25:06 +0600

    Removed github blob. Disable fornever's blog due to its unavailability.

--- .emacs.rc/elfeed-rc.el
+++ .emacs.rc/elfeed-rc.el
@@ -1,9 +1,10 @@
 (rc/require 'elfeed)
 (setq elfeed-feeds
-      '(("http://githubengineering.com/atom.xml")
-        ("https://fornever.me/rss.xml")
+      '(
+        ;; ("https://fornever.me/rss.xml")
         ("http://dsavenko.com/atom.xml")
         ("http://feeds.feedburner.com/xoredhq")
         ("http://blog.4geo.ru/feeds/posts/default")
-        ("http://eev.ee/feeds/atom.xml")))
+        ("http://eev.ee/feeds/atom.xml"))
+      )
 (global-set-key (kbd "C-x w") 'elfeed)
