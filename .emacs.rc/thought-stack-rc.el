a23066a3a1cfa0f6a468b6da5e83b1785430dac3
rexim <reximkut@gmail.com>
2015-07-29 12:24:47 +0600

    Add list command for thought stack feature

--- .emacs.rc/thought-stack-rc.el
+++ .emacs.rc/thought-stack-rc.el
@@ -1,5 +1,7 @@
 (defvar thought-stack '())
 
+(defconst thought-stack-list-buffer-name "*Thought Stack List*")
+
 (defun thought-stack-put (thought)
   (interactive "sWhat are you thinking about? ")
   (setq thought-stack (cons thought thought-stack))
@@ -17,6 +19,17 @@
   (if (not thought-stack)
       (message "[Empty]")
     (message (car thought-stack))))
+
+(defun thought-stack-list ()
+  (interactive)
+  (let ((thought-stack-list-buffer (get-buffer-create thought-stack-list-buffer-name)))
+    (with-current-buffer thought-stack-list-buffer
+      (erase-buffer)
+      (dolist (thought thought-stack)
+        (insert "* TODO ")
+        (insert thought)
+        (insert "\n")))
+    (switch-to-buffer thought-stack-list-buffer)))
 
 (global-set-key (kbd "C-x t p") 'thought-stack-put)
 (global-set-key (kbd "C-x t q") 'thought-stack-pop)
