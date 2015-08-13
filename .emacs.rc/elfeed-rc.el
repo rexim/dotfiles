0d2b171129f6d0e9cb3cf650d43ef80c1510f566
rexim <reximkut@gmail.com>
2015-08-13 09:29:53 +0600

    elfeed: add blog of @portnov

--- .emacs.rc/elfeed-rc.el
+++ .emacs.rc/elfeed-rc.el
@@ -6,6 +6,7 @@
         ("http://feeds.feedburner.com/xoredhq")
         ("http://blog.4geo.ru/feeds/posts/default")
         ("http://eev.ee/feeds/atom.xml")
-        ("https://blog.debiania.in.ua/feeds/all.rss"))
+        ("https://blog.debiania.in.ua/feeds/all.rss")
+        ("https://www.blogger.com/feeds/3628051270185801752/posts/default"))
       )
 (global-set-key (kbd "C-x w") 'elfeed)
