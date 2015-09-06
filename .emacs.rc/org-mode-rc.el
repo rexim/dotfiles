edf72601045116b9f49fadea5c2144dbd12a9a1d
rexim <reximkut@gmail.com>
2015-09-06 23:30:22 +0600

    Use cURL transport for org-cliplink

--- .emacs.rc/org-mode-rc.el
+++ .emacs.rc/org-mode-rc.el
@@ -1,5 +1,6 @@
-
 (rc/require 'org-cliplink)
+(custom-set-variables
+ '(org-cliplink-transport-implementation (quote curl)))
 
 (global-set-key (kbd "C-x a") 'org-agenda)
 (global-set-key (kbd "C-x p i") 'org-cliplink)
