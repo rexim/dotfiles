f86278fac7c8dfab5a4ee72f7c15e4b96aa0de0e
rexim <reximkut@gmail.com>
2015-09-15 09:23:29 +0600

    Configure org-capture template

--- .emacs.rc/org-capture-rc.el
+++ .emacs.rc/org-capture-rc.el
@@ -1,2 +1,4 @@
-(setq org-default-notes-file (concat "~/Documents/Personal/Notes.org"))
+(setq org-capture-templates
+      '(("t" "Todo" entry (file "~/Documents/Personal/Notes.org")
+         "* TODO %?\n  SCHEDULED: %t\n")))
 (define-key global-map "\C-cc" 'org-capture)
