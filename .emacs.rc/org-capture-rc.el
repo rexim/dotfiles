879af85d8be9688eddad3b0f7250c47f97091f7e
rexim <reximkut@gmail.com>
2015-09-15 09:26:24 +0600

    Separate org-capture template for work and for home

--- .emacs.rc/org-capture-rc.el
+++ .emacs.rc/org-capture-rc.el
@@ -1,4 +1,6 @@
 (setq org-capture-templates
-      '(("t" "Todo" entry (file "~/Documents/Personal/Notes.org")
+      '(("t" "Add Personal task" entry (file "~/Documents/Personal/Tasks.org")
+         "* TODO %?\n  SCHEDULED: %t\n")
+        ("i" "Add iTest task" entry (file "~/Documents/Personal/Itest.org")
          "* TODO %?\n  SCHEDULED: %t\n")))
 (define-key global-map "\C-cc" 'org-capture)
