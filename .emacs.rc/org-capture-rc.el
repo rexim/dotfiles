57b241fbba203751676bd7d540ba8bf86f2f3d05
rexim <reximkut@gmail.com>
2015-09-15 17:46:22 +0600

    org-capture: change command for personal notes

--- .emacs.rc/org-capture-rc.el
+++ .emacs.rc/org-capture-rc.el
@@ -1,5 +1,5 @@
 (setq org-capture-templates
-      '(("t" "Add Personal task" entry (file "~/Documents/Personal/Tasks.org")
+      '(("p" "Add Personal task" entry (file "~/Documents/Personal/Tasks.org")
          "* TODO %?\n  SCHEDULED: %t\n")
         ("i" "Add iTest task" entry (file "~/Documents/Personal/Itest.org")
          "* TODO %?\n  SCHEDULED: %t\n")))
