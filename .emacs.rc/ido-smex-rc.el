70839d5784594f2c7108d22a07c7985920d4e5f4
rexim <reximkut@gmail.com>
2015-12-07 14:01:02 +0600

    Enable ido-ubiquitous mode

--- .emacs.rc/ido-smex-rc.el
+++ .emacs.rc/ido-smex-rc.el
@@ -1,6 +1,10 @@
 (rc/require 'smex 'ido-ubiquitous)
 
+(require 'ido-ubiquitous)
+
 (ido-mode 1)
 (ido-everywhere 1)
+(ido-ubiquitous-mode 1)
+
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
