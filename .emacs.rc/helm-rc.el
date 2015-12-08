98bd02b8495c9617f424125da9f051926a7b49d0
rexim <reximkut@gmail.com>
2015-12-08 14:39:15 +0600

    Revert "Use helm instead of smex"
    This reverts commit 2b78018625166d5b84c91a8b562595f4ce6677ba.

--- .emacs.rc/helm-rc.el
+++ .emacs.rc/helm-rc.el
@@ -6,8 +6,3 @@
 (global-set-key (kbd "C-c h g") 'helm-git-grep)
 (global-set-key (kbd "C-c h f") 'helm-find)
 (global-set-key (kbd "C-c h a") 'helm-org-agenda-files-headings)
-
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
