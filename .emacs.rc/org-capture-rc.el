5c7f9aeca7f13c6fce658c00a17cbc0c26ec64b3
rexim <reximkut@gmail.com>
2015-12-30 17:37:08 +0600

    org-cliplink with org-capture

--- .emacs.rc/org-capture-rc.el
+++ .emacs.rc/org-capture-rc.el
@@ -2,5 +2,7 @@
       '(("p" "Add Personal task" entry (file "~/Documents/Personal/Tasks.org")
          "* TODO %?\n  SCHEDULED: %t\n")
         ("i" "Add iTest task" entry (file "~/Documents/Personal/Itest.org")
-         "* TODO %?\n  SCHEDULED: %t\n")))
+         "* TODO %?\n  SCHEDULED: %t\n")
+        ("K" "Personal Cliplink" entry (file "~/Documents/Personal/Tasks.org")
+         "* TODO %(org-cliplink-capture) \n  SCHEDULED: %t\n" :empty-lines 1)))
 (define-key global-map "\C-cc" 'org-capture)
