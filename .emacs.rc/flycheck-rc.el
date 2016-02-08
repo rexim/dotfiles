a90c4829a4b308421c5962c70bbc3c177a55da12
rexim <reximkut@gmail.com>
2016-02-09 01:34:55 +0600

    Enable flycheck only for c/c++

--- .emacs.rc/flycheck-rc.el
+++ .emacs.rc/flycheck-rc.el
@@ -1,3 +1,7 @@
 (rc/require 'flycheck)
 
-(global-flycheck-mode)
+(defun rc/turn-on-flycheck ()
+  (flycheck-mode 1))
+
+(add-hook 'c++-mode 'rc/turn-on-flycheck)
+(add-hook 'c-mode 'rc/turn-on-flycheck)
