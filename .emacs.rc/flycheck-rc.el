1de3b970929f4ef95e2ec3d3e6348ed9aa6b9fde
rexim <reximkut@gmail.com>
2016-02-09 13:23:01 +0600

    Fix flycheck rc

--- .emacs.rc/flycheck-rc.el
+++ .emacs.rc/flycheck-rc.el
@@ -1,7 +1,8 @@
 (rc/require 'flycheck)
 
 (defun rc/turn-on-flycheck ()
+  (interactive)
   (flycheck-mode 1))
 
-(add-hook 'c++-mode 'rc/turn-on-flycheck)
-(add-hook 'c-mode 'rc/turn-on-flycheck)
+(add-hook 'c++-mode-hook 'rc/turn-on-flycheck)
+(add-hook 'c-mode-hook 'rc/turn-on-flycheck)
