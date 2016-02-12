fd0469664928901e4cdba5094b52bb1749f6d8cd
rexim <reximkut@gmail.com>
2016-02-12 13:15:57 +0600

    Add function for enabling c++11 for flycheck

--- .emacs.rc/flycheck-rc.el
+++ .emacs.rc/flycheck-rc.el
@@ -4,5 +4,10 @@
   (interactive)
   (flycheck-mode 1))
 
+(defun rc/enable-c++11-flycheck ()
+  (interactive)
+  (setq flycheck-clang-language-standard "c++11")
+  (setq flycheck-gcc-language-standard "c++11"))
+
 (add-hook 'c++-mode-hook 'rc/turn-on-flycheck)
 (add-hook 'c-mode-hook 'rc/turn-on-flycheck)
