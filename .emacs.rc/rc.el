85b820ea1bd02655fda3180946aa39514709f1bc
rexim <reximkut@gmail.com>
2016-02-20 15:36:47 +0600

    Update MELPA URL

--- .emacs.rc/rc.el
+++ .emacs.rc/rc.el
@@ -1,7 +1,8 @@
+(require 'package)
 (package-initialize)
 
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 ;(add-to-list 'package-archives
 ;             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
