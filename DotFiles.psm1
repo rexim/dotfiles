82cb1077244cc5d1b9b79944158bfc1b668fa73d
rexim <reximkut@gmail.com>
2016-03-17 16:04:40 +0600

    Add Undeploy-Manifest command

--- DotFiles.psm1
+++ DotFiles.psm1
@@ -13,6 +13,21 @@ function Symlink-File {
     }
 }
 
+function Unsymlink-File {
+    $DestPath = $args[0]
+    $SourcePath = $args[1]
+    if (Test-Path $DestPath) {
+        if ((Get-Item $SourcePath) -is [System.IO.DirectoryInfo]) {
+            cmd /c rmdir "$DestPath"
+        } else {
+            cmd /c del "$DestPath"
+        }
+        echo "$DestPath has been unsymlinked"
+    } else {
+        Write-Warning "$DestPath doesn't exist"
+    }
+}
+
 function Deploy-Manifest {
     $ManifestFile = $args[0]
     
@@ -28,6 +43,34 @@ function Deploy-Manifest {
         switch($DeployOp) {
             "symlink" {
                 Symlink-File $DestPath $SourcePath
+            }
+    
+            "copy" {
+                Write-Warning "The 'copy' operation is not implemented yet. Skipping..."
+            }
+    
+            default {
+                Write-Warning "Unknown operation $operation. Skipping..."
+            }
+        }
+    }
+}
+
+function Undeploy-Manifest {
+    $ManifestFile = $args[0]
+    
+    echo "Undeploying $ManifestFile..."
+
+    $Manifest = Import-Csv -Header ("file", "operation") -Delimiter ("|") -Path ".\$ManifestFile"
+    $EmacsHome = $env:HOME
+    foreach ($ManifestRow in $Manifest) {
+        $DeployFile = $ManifestRow.file
+        $DeployOp = $ManifestRow.operation
+        $SourcePath = "$PSScriptRoot\$DeployFile"
+        $DestPath = "$EmacsHome\$DeployFile"
+        switch($DeployOp) {
+            "symlink" {
+                Unsymlink-File $DestPath $SourcePath
             }
     
             "copy" {
