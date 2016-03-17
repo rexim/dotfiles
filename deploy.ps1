814f01d9d0e8fc0839575e0048125d6607b6c9aa
rexim <reximkut@gmail.com>
2016-03-17 15:21:14 +0600

    Decouple Windows deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,46 @@
-﻿$HomeExists = Test-Path Env:HOME
+﻿function Symlink-File {
+    $DestPath = $args[0]
+    if (Test-Path $DestPath) {
+        Write-Warning "$DestPath is already symlinked"
+    } else {
+        if ((Get-Item $SourcePath) -is [System.IO.DirectoryInfo]) {
+            cmd /c mklink /D "$DestPath" "$SourcePath"
+        } else {
+            cmd /c mklink "$DestPath" "$SourcePath"
+        }
+        echo "$DestPath has been symlinked"
+    }
+}
+
+function Deploy-Manifest {
+    $ManifestFile = $args[0]
+    
+    echo "Deploying $ManifestFile..."
+
+    $Manifest = Import-Csv -Header ("file", "operation") -Delimiter ("|") -Path ".\$ManifestFile"
+    $EmacsHome = $env:HOME
+    foreach ($ManifestRow in $Manifest) {
+        $DeployFile = $ManifestRow.file
+        $DeployOp = $ManifestRow.operation
+        $SourcePath = "$PSScriptRoot\$DeployFile"
+        $DestPath = "$EmacsHome\$DeployFile"
+        switch($DeployOp) {
+            "symlink" {
+                Symlink-File "$DestPath"
+            }
+    
+            "copy" {
+                Write-Warning "The 'copy' operation is not implemented yet. Skipping..."
+            }
+    
+            default {
+                Write-Warning "Unknown operation $operation. Skipping..."
+            }
+        }
+    }
+}
+
+$HomeExists = Test-Path Env:HOME
 if ($HomeExists -ne $True) {
     echo "Creating HOME environment variable and targeting it to $env:USERPROFILE"
     [Environment]::SetEnvironmentVariable("HOME", $env:USERPROFILE, "User")
@@ -6,35 +48,6 @@ if ($HomeExists -ne $True) {
     Write-Warning "HOME environment variable already exists. Not modifying the existing value."
 }
 
-$Manifest = Import-Csv -Header ("file", "operation") -Delimiter ("|") -Path .\MANIFEST
-$EmacsHome = $env:HOME
-foreach ($ManifestRow in $Manifest) {
-    $DeployFile = $ManifestRow.file
-    $DeployOp = $ManifestRow.operation
-    $SourcePath = "$PSScriptRoot\$DeployFile"
-    $DestPath = "$EmacsHome\$DeployFile"
-    switch($DeployOp) {
-        "symlink" {
-            if (Test-Path $DestPath) {
-                Write-Warning "$DestPath is already symlinked"
-            } else {
-                if ((Get-Item $SourcePath) -is [System.IO.DirectoryInfo]) {
-                    cmd /c mklink /D "$DestPath" "$SourcePath"
-                } else {
-                    cmd /c mklink "$DestPath" "$SourcePath"
-                }
-                echo "$DestPath has been symlinked"
-            }
-        }
-
-        "copy" {
-            Write-Warning "The 'copy' operation is not implemented yet. Skipping..."
-        }
-
-        default {
-            Write-Warning "Unknown operation $operation. Skipping..."
-        }
-    }
-}
+Deploy-Manifest MANIFEST
 
 pause
