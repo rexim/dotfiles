99bdbf56fcc132948988a8e745054c6ef4feed77
rexim <reximkut@gmail.com>
2016-03-21 23:01:00 +0600

    emacs: open xlsx files with libreoffice

--- .emacs.rc/openwith-rc.el
+++ .emacs.rc/openwith-rc.el
@@ -2,11 +2,14 @@
 
 (openwith-mode 1)
 
-(when (eq system-type 'gnu/linux)
-  (setq openwith-associations
-        '(("\\.pdf\\'" "evince" (file))
-          ("\\.djvu\\'" "evince" (file))
-          ("\\.docx\\'" "libreoffice" (file))
-          ("\\.doc\\'" "libreoffice" (file))
-          ("\\.rtf\\'" "libreoffice" (file))
-          ("\\.mp3\\'" "vlc" (file)))))
+
+(setq openwith-associations
+      (append
+       (when (eq system-type 'gnu/linux)
+         '(("\\.pdf\\'" "evince" (file))
+           ("\\.djvu\\'" "evince" (file))))
+       '(("\\.docx\\'" "libreoffice" (file))
+         ("\\.xlsx\\'" "libreoffice" (file))
+         ("\\.doc\\'" "libreoffice" (file))
+         ("\\.rtf\\'" "libreoffice" (file))
+         ("\\.mp3\\'" "vlc" (file)))))
