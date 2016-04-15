fa862ea07268677512b0f103a0fb11d8b68f28d3
rexim <reximkut@gmail.com>
2016-04-15 15:35:13 +0600

    Don't care 'bout merlin if no opam

--- .emacs.rc/merlin-rc.el
+++ .emacs.rc/merlin-rc.el
@@ -1,12 +1,17 @@
-(setq opam-share (substring
-                  (shell-command-to-string "opam config var share 2> /dev/null")
-                  0 -1))
-(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
-(require 'merlin)
 
-(add-hook 'tuareg-mode-hook 'merlin-mode t)
-(add-hook 'caml-mode-hook 'merlin-mode t)
+(let* ((opam-executable (executable-find "opam"))
+       (opam-config-command (when opam-executable
+                              (concat opam-executable
+                                      " config var share 2> /dev/null")))
+       (opam-share (when opam-config-command
+                     (substring (shell-command-to-string opam-config-command)
+                                0 -1))))
+  (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+  (when (require 'merlin)
 
-(setq merlin-use-auto-complete-mode 'easy)
+    (add-hook 'tuareg-mode-hook 'merlin-mode t)
+    (add-hook 'caml-mode-hook 'merlin-mode t)
 
-(setq merlin-command 'opam)
+    (setq merlin-use-auto-complete-mode 'easy)
+
+    (setq merlin-command 'opam)))
