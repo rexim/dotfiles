d8fbf7fc5cb3fab52bacbfc266f40040a2a9962d
rexim <reximkut@gmail.com>
2016-04-19 20:28:59 +0600

    Prettify whitespace-style value

--- .emacs.rc/whitespace-mode-rc.el
+++ .emacs.rc/whitespace-mode-rc.el
@@ -3,7 +3,9 @@
   (whitespace-mode 1))
 
 (custom-set-variables
- '(whitespace-style (quote (face tabs spaces trailing space-before-tab newline indentation empty space-after-tab space-mark tab-mark newline-mark))))
+ '(whitespace-style (quote (face tabs spaces trailing space-before-tab
+                                 newline indentation empty space-after-tab
+                                 space-mark tab-mark newline-mark))))
 
 (add-hook 'tuareg-mode-hook 'rc/turn-on-whitespace-mode)
 (add-hook 'c++-mode-hook 'rc/turn-on-whitespace-mode)
