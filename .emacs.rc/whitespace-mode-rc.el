93e2f6b8dd7d263951d4da8a2586e6a9d465e212
rexim <reximkut@gmail.com>
2016-04-19 20:44:12 +0600

    Delete trailing whitespace on save

--- .emacs.rc/whitespace-mode-rc.el
+++ .emacs.rc/whitespace-mode-rc.el
@@ -2,6 +2,10 @@
   (interactive)
   (whitespace-mode 1))
 
+(defun rc/delete-trailing-whitespace-on-save ()
+  (interactive)
+  (add-to-list 'write-file-functions 'delete-trailing-whitespace))
+
 (custom-set-variables
  '(whitespace-style (quote (face tabs spaces trailing space-before-tab
                                  newline indentation empty space-after-tab
@@ -10,3 +14,5 @@
 (add-hook 'tuareg-mode-hook 'rc/turn-on-whitespace-mode)
 (add-hook 'c++-mode-hook 'rc/turn-on-whitespace-mode)
 (add-hook 'emacs-lisp-mode-hook 'rc/turn-on-whitespace-mode)
+
+(add-hook 'tuareg-mode-hook 'rc/delete-trailing-whitespace-on-save)
