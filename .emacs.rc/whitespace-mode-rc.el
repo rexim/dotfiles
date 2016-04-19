c6eec93969dc6e71af9d1fe5095b1c1844286a5e
rexim <reximkut@gmail.com>
2016-04-20 03:02:56 +0600

    Combine whitespace-mode and removing trailing whitespaces

--- .emacs.rc/whitespace-mode-rc.el
+++ .emacs.rc/whitespace-mode-rc.el
@@ -6,13 +6,17 @@
   (interactive)
   (add-to-list 'write-file-functions 'delete-trailing-whitespace))
 
+(defun rc/set-up-whitespace-handling ()
+  (interactive)
+  (rc/turn-on-whitespace-mode)
+  (rc/delete-trailing-whitespace-on-save))
+
 (custom-set-variables
  '(whitespace-style (quote (face tabs spaces trailing space-before-tab
                                  newline indentation empty space-after-tab
                                  space-mark tab-mark newline-mark))))
 
-(add-hook 'tuareg-mode-hook 'rc/turn-on-whitespace-mode)
-(add-hook 'c++-mode-hook 'rc/turn-on-whitespace-mode)
-(add-hook 'emacs-lisp-mode-hook 'rc/turn-on-whitespace-mode)
-
-(add-hook 'tuareg-mode-hook 'rc/delete-trailing-whitespace-on-save)
+(add-hook 'tuareg-mode-hook 'rc/set-up-whitespace-handling)
+(add-hook 'c++-mode-hook 'rc/set-up-whitespace-handling)
+(add-hook 'emacs-lisp-mode-hook 'rc/set-up-whitespace-handling)
+(add-hook 'java-mode-hook 'rc/set-up-whitespace-handling)
