e863c8fc8013d529fe191641bde2b1798311e939
rexim <reximkut@gmail.com>
2016-07-12 18:49:11 +0600

    [Emacs]: remove lua company backend

--- .emacs.rc/company-rc.el
+++ .emacs.rc/company-rc.el
@@ -1,12 +1,5 @@
 (rc/require 'company)
-(rc/require 'company-lua)
 
 (require 'company)
-(require 'company-lua)
-
-(custom-set-variables
- '(company-lua-interpreter 'love))
-
-(add-to-list 'company-backends 'company-lua)
 
 (global-company-mode)
