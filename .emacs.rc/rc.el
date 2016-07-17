18110ce34e1dd3ffd883bcf466bd71bf41b027b5
rexim <reximkut@gmail.com>
2016-07-17 23:01:25 +0600

    [Emacs]: add stable melpa repo

--- .emacs.rc/rc.el
+++ .emacs.rc/rc.el
@@ -3,6 +3,8 @@
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 ;(add-to-list 'package-archives
 ;             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
