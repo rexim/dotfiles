2630eb236670a596ec8b96beb58632e6543a07ec
rexim <reximkut@gmail.com>
2016-10-11 21:55:44 +0600

    [Emacs] implement update C++ guard function

--- .emacs.rc/ccmode-rc.el
+++ .emacs.rc/ccmode-rc.el
@@ -4,3 +4,33 @@
                                 (other . "bsd")))
 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+(defun rc/guard-from-path (file-path)
+  (let* ((cmps (-> file-path
+                   (split-string "[/\\.]")
+                   (reverse)))
+         (result nil)
+         (guard ""))
+    (while (and cmps
+                (not (string= (car cmps) "src")))
+      (push (car cmps) result)
+      (pop cmps))
+
+    (while result
+      (setq guard (concat guard (car result) "_"))
+      (pop result))
+
+    (upcase guard)))
+
+(defun rc/update-include-guard ()
+  (interactive)
+  (let* ((file-path (buffer-file-name))
+         (guard (rc/guard-from-path file-path)))
+    (goto-char 0)
+    (forward-word)
+    (forward-char)
+    (let ((begin (point)))
+      (end-of-line)
+      (let ((end (point)))
+        (goto-char 0)
+        (replace-string (buffer-substring begin end) guard)))))
