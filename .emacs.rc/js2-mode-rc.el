72a4c4443f8f0e837f401f6b349486a50c1b1389
rexim <reximkut@gmail.com>
2016-10-24 10:59:41 +0600

    [Emacs] Setup interpreter for js2-mode

--- .emacs.rc/js2-mode-rc.el
+++ .emacs.rc/js2-mode-rc.el
@@ -1,2 +1,5 @@
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+(add-to-list 'interpreter-mode-alist '("node". js2-jsx-mode))
