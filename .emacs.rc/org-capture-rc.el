bc79766726b660974ad8aeaf09132f5407a5a041
rexim <reximkut@gmail.com>
2016-10-30 16:19:22 +0600

    Remove obsolete capture shortcuts

--- .emacs.rc/org-capture-rc.el
+++ .emacs.rc/org-capture-rc.el
@@ -3,8 +3,6 @@
          "* TODO %?\n  SCHEDULED: %t\n")
         ("s" "Add Stream task" entry (file "~/Documents/Agenda/Stream.org")
          "* TODO %?\n  SCHEDULED: %t\n")
-        ("i" "Add iTest task" entry (file "~/Documents/Agenda/Itest.org")
-         "* TODO %?\n  SCHEDULED: %t\n")
         ("K" "Personal Cliplink" entry (file "~/Documents/Agenda/Tasks.org")
          "* TODO %(org-cliplink-capture) \n  SCHEDULED: %t\n" :empty-lines 1)))
 (define-key global-map "\C-cc" 'org-capture)
