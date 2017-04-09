d4c776d9ce23616d4fe92d7c03346555cf593237
rexim <reximkut@gmail.com>
2017-04-10 04:40:08 +0700

    [Emacs] Remove unused capture templates

--- .emacs.rc/org-capture-rc.el
+++ .emacs.rc/org-capture-rc.el
@@ -1,8 +1,6 @@
 (setq org-capture-templates
-      '(("p" "Add Personal task" entry (file "~/Documents/Agenda/Tasks.org")
+      '(("p" "Capture task" entry (file "~/Documents/Agenda/Tasks.org")
          "* TODO %?\n  SCHEDULED: %t\n")
-        ("s" "Add Stream task" entry (file "~/Documents/Agenda/Stream.org")
-         "* TODO %?\n  SCHEDULED: %t\n")
-        ("K" "Personal Cliplink" entry (file "~/Documents/Agenda/Tasks.org")
+        ("K" "Cliplink capture task" entry (file "~/Documents/Agenda/Tasks.org")
          "* TODO %(org-cliplink-capture) \n  SCHEDULED: %t\n" :empty-lines 1)))
 (define-key global-map "\C-cc" 'org-capture)
