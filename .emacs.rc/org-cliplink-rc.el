824f19e23720a7e3a7d3db95c445ae85b14f5638
rexim <reximkut@gmail.com>
2017-08-16 20:40:43 +0700

    [Emacs] Use url-el transport from now on

--- .emacs.rc/org-cliplink-rc.el
+++ .emacs.rc/org-cliplink-rc.el
@@ -2,7 +2,7 @@
 
 (when (eq system-type 'gnu/linux)
   (custom-set-variables
-   '(org-cliplink-transport-implementation (quote curl))))
+   '(org-cliplink-transport-implementation (quote url-el))))
 
 (global-set-key (kbd "C-x p i") 'org-cliplink)
 
