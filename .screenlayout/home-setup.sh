38f4f714d3a19fdbe269f624e7fc02ca766ab55a
rexim <reximkut@gmail.com>
2017-10-24 02:34:28 +0700

    Revert "[screenlayout] Update the monitor names"
    This reverts commit 0bf443f942b4e5fe465742ea9e41d7d5779e7ea0.

--- .screenlayout/home-setup.sh
+++ .screenlayout/home-setup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output eDP-1 --mode 1366x768 --pos 1920x312 --rotate normal --output HDMI-2 --off
+xrandr --output HDMI2 --off --output HDMI1 --off --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --mode 1366x768 --pos 1920x312 --rotate normal --output VIRTUAL1 --off
