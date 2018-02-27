f7c87e36bef0f2a2df6c7e67c30a792dfbac4626
rexim <reximkut@gmail.com>
2018-02-27 17:12:41 +0700

    [screenlayout] add my HDMI monitor

---
+++ .screenlayout/home-setup-2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --mode 1366x768 --pos 1920x312 --rotate normal --output VIRTUAL1 --off
