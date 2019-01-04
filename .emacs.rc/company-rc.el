2260381f2979c4242c8dbbd32b349201ff4e053c
rexim <reximkut@gmail.com>
2019-01-05 01:37:58 +0700

    [Emacs] add company-lua

--- .emacs.rc/company-rc.el
+++ .emacs.rc/company-rc.el
@@ -1,5 +1,17 @@
 (rc/require 'company)
+(rc/require 'company-lua)
 
 (require 'company)
+(require 'company-lua)
+
+(defun rc/lua-mode-company-init ()
+  (interactive)
+  (setq-local company-backends '((company-lua
+                                  company-etags
+                                  company-dabbrev-code
+                                  company-yasnippet)))
+  (setq-local company-lua-interpreter 'love))
+
+(add-hook 'lua-mode-hook 'rc/lua-mode-company-init)
 
 (global-company-mode)
