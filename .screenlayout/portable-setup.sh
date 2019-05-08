aa025055e066ac29bd686df03b59c670787a54b9
rexim <reximkut@gmail.com>
2019-05-09 06:47:39 +0700

    [screenlayout] Update portable-setup

--- .screenlayout/portable-setup.sh
+++ .screenlayout/portable-setup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI2 --off --output HDMI1 --off --output DP1 --off --output eDP1 --mode 1366x768 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output DP-1 --off --output HDMI-1 --off --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-2 --off
