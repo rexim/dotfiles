c9a76d9ff4c293dbf031df9eaa8f66e342dc2c03
rexim <reximkut@gmail.com>
2019-06-03 23:30:00 +0700

    [Emacs] use cabal-new-repl for haskell projects

--- .emacs.rc/haskell-mode-rc.el
+++ .emacs.rc/haskell-mode-rc.el
@@ -1,6 +1,6 @@
 (rc/require 'haskell-mode)
 
-(setq haskell-process-type 'cabal-repl)
+(setq haskell-process-type 'cabal-new-repl)
 (setq haskell-process-log t)
 
 (add-hook 'haskell-mode-hook 'haskell-indent-mode)
