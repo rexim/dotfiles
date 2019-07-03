d383b4a115512401f21eb9a26e82204467e5b023
rexim <reximkut@gmail.com>
2019-07-03 22:12:07 +0700

    [Emacs] remove dead code

--- .emacs.rc/ccmode-rc.el
+++ .emacs.rc/ccmode-rc.el
@@ -5,24 +5,3 @@
 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
-(defun rc/guard-from-path (file-path stop-folder)
-  (->> (split-string file-path "[/\\.]")
-       (reverse)
-       (-take-while (-lambda (x) (not (string= stop-folder x))))
-       (-reduce-from (-lambda (acc x) (concat (upcase x) "_" acc)) "")))
-
-
-(defun rc/update-include-guard (stop-folder)
-  (interactive "sStop folder [src]: ")
-  (let* ((file-path (buffer-file-name))
-         (guard (rc/guard-from-path file-path (if (= 0 (length stop-folder))
-                                                  "src"
-                                                stop-folder))))
-    (goto-char 0)
-    (forward-word)
-    (forward-char)
-    (let ((begin (point)))
-      (end-of-line)
-      (let ((end (point)))
-        (goto-char 0)
-        (replace-string (buffer-substring begin end) guard)))))
