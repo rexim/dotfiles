f96f2ad507a4a6035e8dfdc1d38af4c2aa52d730
rexim <reximkut@gmail.com>
2019-12-24 19:16:58 +0700

    [screenlayout] Change primary screen for B590

--- .screenlayout/home-setup-b590.sh
+++ .screenlayout/home-setup-b590.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS-1 --primary --mode 1366x768 --pos 1920x312 --rotate normal --output DP-1 --off --output HDMI-1 --off --output VGA-1 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output LVDS-1 --mode 1366x768 --pos 1920x312 --rotate normal --output DP-1 --off --output HDMI-1 --off --output VGA-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
