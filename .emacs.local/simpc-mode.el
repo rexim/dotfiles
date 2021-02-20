8c822ec1e683c0123ed1bbd90e98087b58fc1992
rexim <reximkut@gmail.com>
2021-02-21 01:18:24 +0700

    [Emacs] add simpc-mode

---
+++ .emacs.local/simpc-mode.el
@@ -0,0 +1,86 @@
+(require 'subr-x)
+
+(defvar simpc-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    ;; C/C++ style comments
+	(modify-syntax-entry ?/ ". 124b" table)
+	(modify-syntax-entry ?* ". 23" table)
+	(modify-syntax-entry ?\n "> b" table)
+    ;; Preprocessor stuff?
+    (modify-syntax-entry ?# "." table)
+    ;; Chars are the same as strings
+    (modify-syntax-entry ?' "\"" table)
+    ;; Treat <> as punctuation (needed to highlight C++ keywords
+    ;; properly in template syntax)
+    (modify-syntax-entry ?< "." table)
+    (modify-syntax-entry ?> "." table)
+    table))
+
+(defun simpc-keywords ()
+  '("auto" "break" "case" "char" "const" "continue" "default" "do" "double"
+    "else" "enum" "extern" "float" "for" "goto" "if" "int" "long" "register"
+    "return" "short" "signed" "sizeof" "static" "struct" "switch" "typedef"
+    "union" "unsigned" "void" "volatile" "while" "alignas" "alignof" "and"
+    "and_eq" "asm" "atomic_cancel" "atomic_commit" "atomic_noexcept" "bitand"
+    "bitor" "bool" "catch" "char16_t" "char32_t" "char8_t" "class" "co_await"
+    "co_return" "co_yield" "compl" "concept" "const_cast" "consteval" "constexpr"
+    "constinit" "decltype" "delete" "dynamic_cast" "explicit" "export" "false" 
+    "friend" "inline" "mutable" "namespace" "new" "noexcept" "not" "not_eq"
+    "nullptr" "operator" "or" "or_eq" "private" "protected" "public" "reflexpr"
+    "reinterpret_cast" "requires" "static_assert" "static_cast" "synchronized"
+    "template" "this" "thread_local" "throw" "true" "try" "typeid" "typename"
+    "using" "virtual" "wchar_t" "xor" "xor_eq"))
+
+(defun simpc-font-lock-keywords ()
+  (list
+   `("# *[a-zA-Z0-9_]+" . font-lock-preprocessor-face)
+   `("#.*include \\(\\(<\\|\"\\).*\\(>\\|\"\\)\\)" . (1 font-lock-string-face))
+   `(,(regexp-opt (simpc-keywords) 'symbols) . font-lock-keyword-face)))
+
+;;; TODO: try to replace simpc--space-prefix-len with current-indentation
+(defun simpc--space-prefix-len (line)
+  (- (length line)
+     (length (string-trim-left line))))
+
+(defun simpc--previous-non-empty-line ()
+  (save-excursion
+    (forward-line -1)
+    (while (and (not (bobp))
+                (string-empty-p
+                 (string-trim-right
+                  (thing-at-point 'line t))))
+      (forward-line -1))
+    (thing-at-point 'line t)))
+
+(defun simpc--desired-indentation ()
+  (let ((cur-line (string-trim-right (thing-at-point 'line t)))
+        (prev-line (string-trim-right (simpc--previous-non-empty-line)))
+        (indent-len 4))
+    (cond
+     ((and (string-suffix-p "{" prev-line)
+           (string-prefix-p "}" (string-trim-left cur-line)))
+      (simpc--space-prefix-len prev-line))
+     ((string-suffix-p "{" prev-line)
+      (+ (simpc--space-prefix-len prev-line) indent-len))
+     ((string-prefix-p "}" (string-trim-left cur-line))
+      (max (- (simpc--space-prefix-len prev-line) indent-len) 0))
+     (t (simpc--space-prefix-len prev-line)))))
+
+;;; TODO: customizable indentation (amount of spaces, tabs, etc)
+(defun simpc-indent-line ()
+  (interactive)
+  (when (not (bobp))
+    (let* ((current-indentation
+            (simpc--space-prefix-len (thing-at-point 'line t)))
+           (desired-indentation
+            (simpc--desired-indentation))
+           (n (max (- (current-column) current-indentation) 0)))
+      (indent-line-to desired-indentation)
+      (forward-char n))))
+
+(define-derived-mode simpc-mode prog-mode "Simple C"
+  "Simple major mode for editing C files."
+  :syntax-table simpc-mode-syntax-table
+  (setq-local font-lock-defaults '(simpc-font-lock-keywords))
+  (setq-local indent-line-function 'simpc-indent-line)
+  (setq-local comment-start "// "))
