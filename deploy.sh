bfd175308ebd99daefde8a0a63468ed6ee37c4e3
rexim <reximkut@gmail.com>
2024-09-25 03:24:55 +0700

    [deploy] unnest checks

--- deploy.sh
+++ deploy.sh
@@ -7,36 +7,40 @@ symlinkFile() {
     destination="$HOME/$2/$1"
 
     mkdir -p $(dirname "$destination")
-    
-    if [ ! -L "$destination" ]; then
-        if [ -e "$destination" ]; then
-            echo "[ERROR] $destination exists but it's not a symlink. Please fix that manually" && exit 1
-        else
-            ln -s "$filename" "$destination"
-            echo "[OK] $filename -> $destination"
-        fi
-    else
+
+    if [ -L "$destination" ]; then
         echo "[WARNING] $filename already symlinked"
+        return
     fi
+
+    if [ -e "$destination" ]; then
+        echo "[ERROR] $destination exists but it's not a symlink. Please fix that manually"
+        exit 1
+    fi
+
+    ln -s "$filename" "$destination"
+    echo "[OK] $filename -> $destination"
 }
 
 deployManifest() {
     for row in $(cat $SCRIPT_DIR/$1); do
+        if [[ ! "$row" =~ ^#.* ]]; then
+            continue
+        fi
+
         filename=$(echo $row | cut -d \| -f 1)
         operation=$(echo $row | cut -d \| -f 2)
         destination=$(echo $row | cut -d \| -f 3)
 
-        if [[ ! "$row" =~ ^#.* ]]; then
-            case $operation in
-                symlink)
-                    symlinkFile $filename $destination
-                    ;;
+        case $operation in
+            symlink)
+                symlinkFile $filename $destination
+                ;;
 
-                *)
-                    echo "[WARNING] Unknown operation $operation. Skipping..."
-                    ;;
-            esac
-        fi
+            *)
+                echo "[WARNING] Unknown operation $operation. Skipping..."
+                ;;
+        esac
     done
 }
 
